f34a3fe51e48d0529e63bfdd11672e80013e0af9
David <jangdawit@gmail.com>
2026-08-30 22:48:25 +0900

    feat: Swagger admin 그룹에 X-Admin-Token Authorize 지원

--- src/main/java/com/optrip/server/config/OpenApiConfig.java
+++ src/main/java/com/optrip/server/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.optrip.server.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,11 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("AdminToken",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("X-Admin-Token")))
                 .info(new Info()
                         .title("Optrip Server API")
                         .description("Optrip 백엔드 API 명세")
@@ -31,6 +39,9 @@ public class OpenApiConfig {
         return GroupedOpenApi.builder()
                 .group("admin")
                 .pathsToMatch("/admin/**")
+                .addOpenApiCustomizer(openApi -> openApi.getPaths().values().forEach(pathItem ->
+                        pathItem.readOperations().forEach(operation ->
+                                operation.addSecurityItem(new SecurityRequirement().addList("AdminToken")))))
                 .build();
     }
 }
